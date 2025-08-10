f3ca7d19d233e080718648d0c8fa20dfcee1f111
Mohammed Amine Aragrag <mohammedaminearagrag@gmail.com>
2025-08-10 15:19:58 +0100

    Remove development script and clean up project

--- run_dev.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Development server runner with reload functionality
-"""
-
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
